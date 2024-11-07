ebb0d3ee7632d33f78d5685ccda6373f0f3b9b6f
kwakjaehun <jh5414092@icloud.com>
2024-11-07 23:30:21 +0900

    save

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,6 @@
 import Avatar from "@/components/atoms/Avatar";
 import SVGIcon from "@/components/atoms/SVGIcon";
+import Header from "@/components/molecules/Header/Header";
 
 export default function Home() {
   return (
@@ -7,6 +8,7 @@ export default function Home() {
       <h1 className="text-common-100 text-title1">hello</h1>
       <SVGIcon icon="Setting" color="gray" size={24} />
       <Avatar shape={"square"} size={"xl"} />
+      <Header intent="main" title="AI" />
     </main>
   );
 }
--- src/components/molecules/Header/Header.tsx
+++ src/components/molecules/Header/Header.tsx
@@ -1,31 +1,40 @@
+import Avatar from "@/components/atoms/Avatar";
 import Paragraph from "@/components/atoms/Paragraph/Paragraph";
 import SVGIcon from "@/components/atoms/SVGIcon";
 import { PropsWithChildren } from "react";
 
 interface HeaderProps {
   intent: "main" | "labelWithIcon" | "label";
+  title?: string;
 }
 
-const Header = ({ intent, children }: PropsWithChildren<HeaderProps>) => {
+const Header = ({
+  intent,
+  children,
+  title = "title",
+}: PropsWithChildren<HeaderProps>) => {
   return (
     <div className="flex justify-between w-full h-[58px] border-b border-line-normal">
       {intent === "main" ? (
-        <div>
-          <div className="flex justify-center items-center w-9 h-[26px] text-title1 font-semibold">
-            LOGO
+        <>
+          <div>
+            <div className="flex justify-center items-center w-9 h-[26px] text-title1 font-semibold">
+              LOGO
+            </div>
+            <h2 className="text-body1 font-semibold"></h2>
           </div>
-          <h2 className="text-body1 font-semibold"></h2>
-        </div>
+        </>
       ) : (
         <>
           <button>
             <SVGIcon icon={"Menu"} />
           </button>
           {intent === "label" ? (
-            <Paragraph>Label</Paragraph>
+            <Paragraph>{title}</Paragraph>
           ) : (
-            <div>
-              <div className="" id="icon-dummy"></div>
+            <div className="flex gap-2">
+              <Avatar size={"xs"} shape={"square"} />
+              <Paragraph>{title}</Paragraph>
             </div>
           )}
         </>
