df5f49a5388599b75829a131d3749861e4947659
kwakjaehun <jh5414092@icloud.com>
2024-11-07 21:39:24 +0900

    Button 제작

---
+++ src/components/atoms/Button/Button.tsx
@@ -0,0 +1,36 @@
+import { cva, VariantProps } from "class-variance-authority";
+import { ButtonHTMLAttributes, PropsWithChildren } from "react";
+
+const ButtonVariants = cva("", {
+  variants: {
+    intent: {
+      greenFill: "text-static-white bg-primary-heavy",
+      greenWeak: "",
+    },
+    size: {
+      large: "text-heading2 font-semibold",
+    },
+  },
+  defaultVariants: {
+    intent: "greenFill",
+  },
+});
+
+interface ButtonProps
+  extends VariantProps<typeof ButtonVariants>,
+    ButtonHTMLAttributes<HTMLButtonElement> {}
+
+const Button = ({
+  children,
+  intent,
+  size,
+  ...props
+}: PropsWithChildren<ButtonProps>) => {
+  return (
+    <button className={ButtonVariants({ intent, size })} {...props}>
+      {children}
+    </button>
+  );
+};
+
+export default Button;
