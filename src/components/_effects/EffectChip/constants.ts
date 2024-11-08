18a251f9dd37d8cdde86b2c6556431575d4fe1e3
kwakjaehun <jh5414092@icloud.com>
2024-11-08 19:44:14 +0900

    Feat: Add storybook.ts about shadow and neon effects

---
+++ src/components/_effects/EffectChip/constants.ts
@@ -0,0 +1,11 @@
+export const EffectStyles = {
+  dropShadowNormal: "drop-shadow-normal",
+  dropShadowEmphasize: "drop-shadow-emphasize",
+  dropShadowStrong: "drop-shadow-strong",
+  dropShadowHeavy: "drop-shadow-heavy",
+  neonLight: "neon-light",
+  neonWeak: "neon-weak",
+  neonNormal: "neon-normal",
+  neonStrong: "neon-strong",
+  neonRed: "neon-red",
+};
