44ed53a5a92e9962ebb4ec748a4ada67534fc90d
lsy8181 <a4630486@gmail.com>
2024-11-10 02:51:11 +0900

    iconbutton 완성

---
+++ src/stories/components/atoms/IconButton.stories.ts
@@ -0,0 +1,32 @@
+import type { Meta, StoryObj } from "@storybook/react";
+
+import IconButton from "@/components/atoms/IconButton";
+
+const meta = {
+  title: "Atoms/IconButton",
+  component: IconButton,
+  parameters: {
+    layout: "centered",
+  },
+  tags: ["autodocs"],
+  argTypes: {
+    intent: {
+      description: "버튼유형",
+      control: "select",
+      options: ["fill", "white", "light"],
+    },
+    size: {
+      description: "버튼 사이즈",
+      control: "select",
+      options: ["sm", "md", "lg"],
+    },
+  },
+  args: {},
+} satisfies Meta<typeof IconButton>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const Default: Story = {
+  args: {},
+};
