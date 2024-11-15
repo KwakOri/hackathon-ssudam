f23b6a88d2dacb813b4342a8464f0caf6e6c8101
kwakjaehun <jh5414092@icloud.com>
2024-11-16 02:29:35 +0900

    Feat: 폴더 구조 작성

---
+++ src/components/molecules/Modals/ConfirmModal/ConfirmModal.tsx
@@ -0,0 +1,39 @@
+"use client";
+import AnimatedModalFrame from "@/components/molecules/Modals/AnimatedModalFrame";
+import { useModal } from "@/contexts/modal.context/modal.context";
+import { useState } from "react";
+
+interface ConfirmModalProps {
+  onSuccess: () => void;
+  onCancel: () => void;
+  contents: string[];
+  id: string;
+}
+
+const ConfirmModal = ({
+  id,
+  onSuccess,
+  onCancel,
+  contents,
+}: ConfirmModalProps) => {
+  const modal = useModal();
+  const [isVisible, setIsVisible] = useState(true);
+  const handleCloseModal = () => {
+    onCancel();
+    setIsVisible(false);
+    setTimeout(() => {
+      modal.close(id);
+    }, 300);
+  };
+  const handleClickConfirm = () => {
+    onSuccess();
+    setIsVisible(false);
+    setTimeout(() => {
+      modal.close(id);
+    }, 300);
+  };
+
+  return <AnimatedModalFrame isVisible={isVisible}></AnimatedModalFrame>;
+};
+
+export default ConfirmModal;
